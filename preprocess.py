2ec5eb46621708e4175c58d5ab34dfe74c4f5c73
Dain Jung <alal9987@gmail.com>
2018-12-28 14:24:34 +0900

    Argument parsing 추가 (data path, width, height)
    positional arguments:
      data_path   Path of folder containing images
    
    optional arguments:
      -h, --help  show this help message and exit
      --w WIDTH
      --h HEIGHT

--- preprocess.py
+++ preprocess.py
@@ -2,27 +2,32 @@ import numpy as np
 import cv2
 import os
 import scipy.misc
+import argparse
 
-data_path = 'data'
+# python preprocess.py data --w 300 --h 240
+parser = argparse.ArgumentParser()
+parser.add_argument('data_path', help = 'Path of folder containing images', type = str)
+parser.add_argument('--w', dest = 'width', default = 300, type = int)
+parser.add_argument('--h', dest = 'height', default = 240, type = int)
+args = parser.parse_args()
+
+data_path = args.data_path
 path_of_outputs = "preprocessed_data\\"
 data_files = os.listdir(data_path)
-out_height = 300
-out_width = int(out_height*0.75)
+out_width, out_height = args.width, args.height
 
 for img_name in data_files:
     load_name = os.path.join(data_path, img_name)
     img = cv2.imread(load_name)
 
     # resizing
-    print((out_width, out_height))
-    img = scipy.misc.imresize(img, (out_height, out_width))
+    img = scipy.misc.imresize(img, (int(out_width*1.3333), out_width))
     H, W = img.shape[:2]
 
-    #### cut ####
-    print(int(0.4*H),H)
-    img = img[int(0.4*H):,:]
+    # cut
+    img = img[int(H-out_height):, :]
 
-    #### adjust ####
+    # adjust
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     eq = cv2.equalizeHist(gray_img)
 

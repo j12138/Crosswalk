dbda9bd991441508fc3e33d723e299cef6d8941d
Dain Jung <alal9987@gmail.com>
2019-01-25 10:00:17 +0900

    try high-level implement / write some class methods

--- crosswalk_data.py
+++ crosswalk_data.py
@@ -2,18 +2,26 @@
 
 import cv2
 
-def launch_annotator(self):
-    cv2.namedWindow('tool')
-    cv2.setMouseCallback('tool', draw_point)
-    pass
-
 class CrosswalkData:
 
-    def __init__(self, img_name):
-        self.img_name = img_name
-        self.input_filenme = ""
-        self.meta = {}
-        self.labels = {}
+    def __init__(self, img_file):
+        self.img_file = img_file
+        #self.input_filenme = ""
+        self.img = cv2.imread(img_file)
+        self.meta = { # value is 3rd elem
+                'obs_car': [0, 1, 0],
+                'obs_human': [0, 1, 0],
+                'shadow': [0, 1, 0],
+                'column': [1, 2, 1],
+                'zebra_ratio': [0, 100, 0],
+                # not zebra
+                'out_of_range' : [0, 1, 0],
+                'old' : [0, 1, 0]
+                }
+        self.labels = {
+            'loc': 0.0,
+            'ang': 0.0
+        }
 
     def __display_image(self):
         pass
@@ -21,13 +29,23 @@ class CrosswalkData:
     def __create_track_bars(self):
         pass
 
-    def annotate(self):
-        self.__display_image()
-        self.__create_track_bars()
+    def display_manual_meta(self):
+        for name in self.meta:
+            print(name, self.meta[name][2])
+    
+    def display_labels(self):
+        for name in self.labels:
+            print(name, self.labels[name])
 
+    def make_trackbar(self, winname):
+        for name in self.meta:
+            cv2.createTrackbar(name, winname, self.meta[name][0], self.meta[name][1], lambda x: x)
 
-launch_annotator()
-for img in images:
-    data = CrosswalkData(img)
-    data.annotate()
+    def input_manual_meta(self, winname):
+        for name in self.meta:
+            self.meta[name][2] = cv2.getTrackbarPos(name, winname)
+
+    def input_labels(self, loc, ang):
+        self.labels['loc'] = loc
+        self.labels['ang'] = ang
 

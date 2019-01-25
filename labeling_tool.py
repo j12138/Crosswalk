43b60fbacdc1255bb99d722be815e8619750f63d
Taejoon Byun <taejoon@umn.edu>
2019-01-24 20:06:13 -0600

    Refactoring labeling_tool.py (in progress)

--- labeling_tool.py
+++ labeling_tool.py
@@ -9,6 +9,62 @@ import csv
 import pymongo
 import crosswalk_data as cd
 
+class Annotator(object):
+    """ An Annotator displays input window for each image and lets user draw
+    points (label) for each image data. """
+
+    def __init__(self, img_dir):
+        self.img_dir = img_dir
+        self.is_input_finshed = False
+
+    def launch(self):
+        for img in glob.glob(self.img_dir + '/*.png'):
+            pass # in progress
+            data = cd.CrosswalkData(img_file)
+            self.__initialize_screen()
+            img_to_display = data.img.copy()
+            launch_annotator(img_to_display, drawn_line)
+            data.make_trackbar('tool')
+            
+            while True:
+                cv2.imshow('tool', img_to_dislay)
+                draw_line_and_compute_label(data, img_to_dislay, drawn_line)
+                
+                if self.is_input_finished:
+                    break
+
+                if cv2.waitKey(2) == 32:
+                    break # press 'spacebar' -> turn to next image
+
+            data.input_manual_meta('tool')
+            data.display_manual_meta()
+            data.display_labels()
+            write_on_csv(drawn_line, data)
+
+    def draw_line_and_compute_label(data, visual, drawn_line):
+        if point[0] == 2 and not drawn_line[0]:
+            cv2.line(visual, points[0], points[1], (0, 0, 255), 2)
+            drawn_line[0] = True
+
+        if point[0] == 4 and not drawn_line[1]:
+            cv2.line(visual, points[2], points[3], (0, 0, 255), 2)
+            drawn_line[1] = True
+
+            loc, ang = compute_label(data.img, points)
+            data.input_labels(loc, ang)
+            write_labels_on_screen(visual, loc, ang)
+
+    def __initialize_screen():
+        global done
+        point[0] = 0 
+        for i in range(4):
+            points[i] = (0, 0)
+        drawn_line[0] = False
+        drawn_line[1] = False
+        done = False
+
+
+
 IMG_DIR = './preprocessed_data'
 path_check_img = "annotated_data\\"
 point = [0, (0,0)] # click_cnt, location carrier
@@ -94,7 +150,7 @@ def write_on_csv(drawn_line, data):
             mywriter = csv.writer(csvfile)
             mywriter.writerow([data.img_file, data.labels['loc'], data.labels['ang']])
 
-def initialize_annotating_params():
+def initialize_screen():
     global done
     point[0] = 0 
     for i in range(4):
@@ -112,7 +168,7 @@ def main():
     for img_file in glob.glob(IMG_DIR + '/*.png'):
 
         data = cd.CrosswalkData(img_file)
-        initialize_annotating_params()
+        initialize_screen()
         visual = data.img.copy()
         launch_annotator(visual, drawn_line)
         data.make_trackbar('tool')

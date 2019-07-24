0ae9fd2515c3a69604e858d56587611a9049cc55
kreeos <kanghyunkrislee@gmail.com>
2019-07-24 09:57:14 +0000

    Container fix

--- src/ml/prune.py
+++ src/ml/prune.py
@@ -2,53 +2,60 @@ import numpy as np
 import tensorflow as tf
 from tensorflow_model_optimization.sparsity import keras as sparsity
 #import SimpleModel
-
+from keras.models import load_model
+import os
+from Models.loss import smoothL1
 
 # Backend agnostic way to save/restore models
-# _, keras_file = tempfile.mkstemp('.h5')
-# print('Saving model to: ', keras_file)
-# tf.keras.models.save_model(model, keras_file, include_optimizer=False)
+#_, keras_file = tempfile.mkstemp('.h5')
+#print('Saving model to: ', keras_file)
+#tf.keras.models.save_model(model, keras_file, include_optimizer=False)
 
-#keras_file = 'trainings/Crosswalk_guide_test/SimpleModel.h5'
+keras_file = './trainings/Crosswalk_guide_test/SimpleModel.h5'
+
+print(keras_file)
 # Load the serialized model
-loaded_model = tf.keras.models.load_model(keras_file)
+#loaded_model = tf.keras.models.load_model(keras_file)
+assert os.path.exists(keras_file), "model doesn't exist"
+loaded_model = load_model(keras_file, custom_objects={'smoothL1': smoothL1})
 
-epochs = 4
-end_step = np.ceil(1.0 * num_train_samples / batch_size).astype(np.int32) * epochs
-print(end_step)
+def prune(num_train_samples, batch_size):
+    epochs = 4
+    end_step = np.ceil(1.0 * num_train_samples / batch_size).astype(np.int32) * epochs
+    print(end_step)
 
-new_pruning_params = {
-        'pruning_schedule': sparsity.PolynomialDecay(initial_sparsity=0.50,
-                                                     final_sparsity=0.90,
-                                                     begin_step=0,
-                                                     end_step=end_step,
-                                                     frequecny=100)
-}
+    new_pruning_params = {
+            'pruning_schedule': sparsity.PolynomialDecay(initial_sparsity=0.50,
+                                                         final_sparsity=0.90,
+                                                         begin_step=0,
+                                                         end_step=end_step,
+                                                         frequency=100)
+    }   
 
-new_pruned_model = sparsity.prune_low_magnitude(loaded_model, **new_pruning_params)
-new_pruned_model.summary()
+    new_pruned_model = sparsity.prune_low_magnitude(loaded_model, **new_pruning_params)
+    new_pruned_model.summary()
 
-new_pruned_model.compile(
-        loss=tf.keras.losses.categorical_crossentropy,
-        optimizer='adam',
-        metrics=['accuracy'])
+    new_pruned_model.compile(
+            loss=tf.keras.losses.categorical_crossentropy,
+            optimizer='adam',
+            metrics=['accuracy'])
 
 # Add a pruning step callback to peg the pruning step to the optimizer's
 # step. Also add a callback to add pruning summaries to tensorboard
-callbacks = [
-        sparsity.UpdatePruningStep(),
-        sparisty.PruningSummaries(log_dir=logdir, profile_batch=0)
-]
+    callbacks = [
+            sparsity.UpdatePruningStep(),
+            sparisty.PruningSummaries(log_dir=logdir, profile_batch=0)
+    ]
 
-new_pruned_model.fit(x_train, y_train,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=1,
-        callbacks=callbacks,
-        validation_data=(x_test, y_test))
+    new_pruned_model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            callbacks=callbacks,
+            validation_data=(x_test, y_test))
 
-score = new_pruned_model.evaluate(x_test, y_test, verbose=0)
-print('Test Loss: ', score[0])
-print('Test Accuracy: ', score[1])
+    score = new_pruned_model.evaluate(x_test, y_test, verbose=0)
+    print('Test Loss: ', score[0])
+    print('Test Accuracy: ', score[1])
 
-
+prune(1,1)

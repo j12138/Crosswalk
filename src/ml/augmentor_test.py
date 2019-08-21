c08a0a273a5560c7a110b4057fa710e7a982b570
Dain Jung <alal9987@gmail.com>
2019-08-21 09:08:00 +0900

    tests for augmentor

--- src/ml/augmentor_test.py
+++ src/ml/augmentor_test.py
@@ -1,9 +1,25 @@
 import Augmentor as aug
 import os, glob
 from PIL import Image
+import numpy as np
 
 
-test_dataset = 'test_dataset/data2_1/'
+test_dataset = './test_dataset/data_array/'
+test_img = Image.open(test_dataset + '5a9dcdb4f1a81435985bf73deb0160f9.jpg')
+test_img = np.asarray(test_img, dtype=np.uint8)
+# print(test_img)
+
+
+def make_imgs_array(img_dir):
+    test_imgs = []
+
+    images = glob.glob(os.path.join(img_dir, '*.jpg'))
+    for img in images:
+        # print(img)
+        im = np.asarray(Image.open(img))
+        test_imgs.append(im)
+
+    return test_imgs
 
 
 def convert_none2jpg(img_dir):
@@ -16,7 +32,8 @@ def convert_none2jpg(img_dir):
 
 
 # convert_none2jpg(test_dataset)
-p = aug.Pipeline(test_dataset)
+# img_array = make_imgs_array(test_dataset)
+p = aug.Pipeline()
 
 # p.random_distortion(probability=1.0, grid_width=10,
 #                     grid_height=10, magnitude=1)
@@ -27,4 +44,9 @@ p.shear(probability=0.5, max_shear_left=3, max_shear_right=3)
 p.flip_left_right(probability=0.5)
 p.zoom(probability=0.3, min_factor=1.1, max_factor=1.5)
 
-p.sample(30)
+
+test_array = make_imgs_array('./test_dataset/data_array/')
+
+# print(test_img)
+print(np.asarray([test_img]))
+p.sample_with_array(np.asarray([test_img]))

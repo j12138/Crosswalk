cd0af3d7d864a1ace35278e7ba5c9a36e112176c
Dain Jung <alal9987@gmail.com>
2019-10-11 22:43:55 +0900

    fix errors

--- src/labeling/makenp.py
+++ src/labeling/makenp.py
@@ -6,7 +6,7 @@ import cv2
 from scipy.misc import imread
 import datetime
 import glob
-import os
+import os, sys, copy
 import random
 import argparse
 import math
@@ -16,7 +16,7 @@ from typing import Tuple, List, Dict
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(BASE_DIR, "..", "..")
 
-now = datetime.datetime.now()
+now = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 logger = logging.getLogger('make_numpy')
 
 
@@ -42,8 +42,9 @@ def show_and_pick_filters():
     :return: list of picked filters
     """
     print('\n------- filter lists -------')
+    print(enumerate(filter_list))
     for i, filter_name in enumerate(filter_list):
-        print('[{}] {}'.format(i, filter_name))
+        print('[{}] {}'.format(i + 1, filter_name))
 
     print('----------------------------')
     print('select filters (ex: 1 2 3 4 5)')
@@ -66,19 +67,18 @@ def show_and_pick_filters():
 class DBMS(object):
     """ Database interface """
 
-    db_filename = 'db.json'
-
     def __init__(self, data_dir):
         """
         :param data_dir: path for preprocessed data
         """
-        self.entries = {}           # DB entries. Dictionary of dictionaries
+        self.db_filename = 'db.json'
+        self.entries = {} # DB entries. Dict[Dict]
         self.batch_dirs = glob.glob(os.path.join(data_dir, '*'))
         self.query_list = {}
         self.val_query_list = None
 
-    @staticmethod
-    def __load_db_in_batch(batch_path: str) -> Dict[Dict]:
+    # @staticmethod
+    def __load_db_in_batch(self, batch_path: str) -> Dict[str, Dict]:
         """ A batch refers to a "batch" of dataset collected in a single
         burst. around 100 to a few hundreds of images are contained in a
         batch, typically.
@@ -114,11 +114,19 @@ class DBMS(object):
         conditions
         """
         filtered = copy.copy(self.entries)
+        print(len(filtered))
+
         for filter_name in filter_names:
+
             # iteratively apply selected filters
             _filter = filter_list[filter_name]
-            filtered = [entry for entry in filtered if
-                        _filter(entry) is True]
+
+            filtered = {entry : filtered[entry] for entry in filtered if
+                        (filtered[entry]['is_input_finished'] is True) and
+                        (_filter(filtered[entry]) is True)}
+
+            print(len(filtered))
+
         return filtered.keys()
 
     def get_train_val_keys(self, ratio: float = 0.2) \
@@ -136,7 +144,7 @@ class DBMS(object):
         return key_list[set_aside_cut_index:], key_list[:set_aside_cut_index]
 
     def make_npy(self, keys: List[str], width: int, height: int,
-                 grayscale: bool, output_dir: str, filename_prefix: str):
+                 grayscale: bool, output_dir: str, filename_prefix=''):
         """ Make npy files for training, from query_list
 
         :param keys: List of keys for the database entries
@@ -166,8 +174,10 @@ class DBMS(object):
 
         # npy file name convention
         save_prefix = os.path.join(output_dir, filename_prefix)
-        x_name = os.path.join(output_dir, filename_prefix, '_x.npy')
-        y_name = os.path.join(output_dir, filename_prefix, '_y.npy')
+        x_name = os.path.join(output_dir, filename_prefix + '_x.npy')
+        y_name = os.path.join(output_dir, filename_prefix + '_y.npy')
+
+        print(x_name)
         logger.info("saving at " + x_name)
         np.save(x_name, xs)
         logger.info("saving at " + y_name)
@@ -199,19 +209,24 @@ def make_npy_file(args):
     data_dir = os.path.join(BASE_DIR, args.dataset_dir)
 
     db = DBMS(data_dir)
+    db.load_database()
+
     if args.cross_val:
         logger.info('Generating train/test numpy files with a ratio'
                     'of {}'.format(args.ratio))
         train_keys, val_keys = db.get_train_val_keys(args.ratio)
         db.make_npy(train_keys, args.width, args.height, args.grayscale,
-                    'train')
-        db.make_npy(val_keys, args.width, args.height, args.grayscale, 'val')
+                    args.output_dir, 'train')
+        db.make_npy(val_keys, args.width, args.height, args.grayscale,
+                    args.output_dir, 'val')
     else:
         selected_filters = show_and_pick_filters()
         logger.info("Selected filters: " + str(selected_filters))
         keys = db.filter_data(selected_filters)
+        # db.make_npy(keys, args.width, args.height, args.grayscale,
+        #            'filter{}'.format(len(selected_filters)))
         db.make_npy(keys, args.width, args.height, args.grayscale,
-                    'filter{}'.format(len(selected_filters)))
+                    args.output_dir)
     logger.info('Finished at ' + str(now))
 
 
@@ -235,13 +250,13 @@ def main():
     parser.add_argument('output_dir', type=str, help="numpy output directory")
     parser.add_argument('--width', '-w', type=int, default=150,
                         help="image width")
-    parser.add_argument('--height', '-h', type=int, default=200,
+    parser.add_argument('--height', '-H', type=int, default=200,
                         help="image height")
     parser.add_argument('--grayscale', '-g', action='store_true',
                         help="grayscale image")
     parser.add_argument('--cross-val', '-c', action='store_true',
                         help="Cross validation")
-    parser.add_argument('--ratio', '-r', type=int, default=0.2,
+    parser.add_argument('--ratio', '-r', type=float, default=0.2,
                         help="Set-aside ratio")
     args = parser.parse_args()
     setup_logger(os.path.join(BASE_DIR, args.output_dir, str(now) + '.log'))

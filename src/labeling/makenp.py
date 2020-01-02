688fe864e84bc193b23cfc1b7dcf2bcf0743b3ad
Dain Jung <alal9987@gmail.com>
2020-01-03 07:00:38 +0900

    fix makenp operation / logging

--- src/labeling/makenp.py
+++ src/labeling/makenp.py
@@ -4,40 +4,14 @@ import logging
 from typing import Tuple, List, Dict
 import datetime
 import numpy as np
-from labeling.database import DBMS, get_filter_list
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from database import DBMS, get_filter_list, show_and_pick_filters
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(BASE_DIR, "..", "..")
 
 now = datetime.datetime.now().strftime('%y-%m-%d-%H-%M')
 logger = logging.getLogger('make_numpy')
-
-
-def show_and_pick_filters(filter_list):
-    """ show pre-declared(AT TOP) filter lists and get user's choice.
-    :return: list of picked filters
-    """
-    print('\n------- filter lists -------')
-    print(enumerate(filter_list))
-    for i, filter_name in enumerate(filter_list):
-        print('[{}] {}'.format(i + 1, filter_name))
-
-    print('----------------------------')
-    print('select filters (ex: 1 2 3 4 5)')
-    picked_num = input('└─ here: ')
-    filter_ids = [int(i) for i in picked_num.split(' ')]
-    filter_keys = list(filter7_list.keys())
-    # print(filter_keys)
-
-    picked = []
-    print('\n------- selected filters -------')
-    for filter_id in filter_ids:
-        key = filter_keys[filter_id - 1]
-        print('[{}] {}'.format(filter_id, key))
-        picked.append(key)
-    print('--------------------------------\n')
-
-    return picked
 
 
 def setup_logger(log_file_path: str):
@@ -57,7 +31,7 @@ def make_npy(db, keys: List[str], width: int, height: int,
              grayscale: bool, output_dir: str, filename_prefix=''):
 
     xs, ys = db.get_npy(keys, width, height, grayscale)
-    
+
     # npy file name convention
     x_name = os.path.join(output_dir, filename_prefix + '_x.npy')
     y_name = os.path.join(output_dir, filename_prefix + '_y.npy')
@@ -95,8 +69,10 @@ def make_npy_file(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset_dir', type=str, help="dataset directory")
-    parser.add_argument('output_dir', type=str, help="numpy output directory")
+    parser.add_argument('dataset_dir', type=str, help="dataset directory", \
+                        default='dataset')
+    parser.add_argument('output_dir', type=str, help="numpy output directory", \
+                        default='npy')
     parser.add_argument('--width', '-w', type=int, default=150,
                         help="image width")
     parser.add_argument('--height', '-H', type=int, default=200,
@@ -114,4 +90,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
